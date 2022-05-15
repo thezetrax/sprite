d6aa94ce1b120fb9df0daabf48f7dc5328aa98ab
Zablon Dawit <zabjd22@gmail.com>
2022-05-15 13:58:29 +0300

    feat(sprite): Added functioning REPL

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
-package sprite
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"sprite/repl"
+)
 
 func main() {
-	println("Jello")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Welcome %s to Sprite, a small language with a humble beginning!\n", user.Username)
+	fmt.Printf("Type your commands here.\n")
+	// Starting Sprite REPL
+	repl.Start(os.Stdin, os.Stdout)
 }
